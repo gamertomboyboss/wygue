8896003dcfe98aa64ac16c609613a40da3d3cea1
gamertomboyboss <78607496+gamertomboyboss@users.noreply.github.com>
2021-06-23 17:40:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ player_x = 10; 
+ player_y = 10;
+  block_image_width = 30; 
+  block_image_height = 30;
+   var player_object= "";
+    var block_image_object= "";
+     function player_update()
+      {
+           fabric.Image.fromURL("https://drive.google.com/file/d/1fGz6T2OZeGqzLGfhN1fZ2VffCG-OzWxL/view?usp=sharing", function(Img)
+            {
+                 player_object = Img;
+                  player_object.scaleToWidth(150);
+                   player_object.scaleToHeight(140);
+                    player_object.set({ top:player_y, left:player_x });
+                      
+                    });
+                 } 
+                  
+          
